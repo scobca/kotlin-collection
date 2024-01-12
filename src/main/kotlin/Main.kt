4a1d97c7b04e17433783185a0cf9f9b725b839f9
scobca <scobca18@yandex.ru>
2024-01-13 02:10:43 +0300

    first lesson: variables and types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+const val helloMessage = "Hello world!"
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun calculator() {
+    val number1 = 124
+    val number2 = 84
+
+    println(number1 + number2)
+}
+
+fun main(args: Array<String>) {
+    println(helloMessage)
+
+    //var = let; val = const
+
+    var age: Number = 10
+    val name: String = "Alex"
+    val isAdmin: Boolean = false
+
+    age = 20
+    println("Ваш возраст: $age, ваше имя: $name, isAdmin: $isAdmin")
+
+
+    // if you want to set null to variable - you need to set ? after variable type
+
+    val nullExample: Number? = null
+    println(nullExample)
+
+    // if you want to call external function just write it like this
+    calculator()
 }
